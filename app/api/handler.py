dc019823472e25cc3a471f54859ffbcdb4e23945
huangwx <hwx.simle@gmail.com>
2017-06-02 22:51:23 +0800

    all programs is ok, next is to add design

--- app/api/handler.py
+++ app/api/handler.py
@@ -5,6 +5,11 @@ from .util import get_result_url_by_name
 
 @api_blueprint.route('/img_by_name', methods=['POST'])
 def index():
-    name = request.form['name']
-    result, bg = get_result_url_by_name(name)
-    return jsonify({'result': result, 'bg': bg})
+    try:
+        name = request.form['name']
+        if name is None or name == '':
+            raise ValueError
+        result, bg = get_result_url_by_name(name)
+        return jsonify({'result': result, 'bg': bg})
+    except ValueError:
+        return 'ValueError', 400
--- app/main/handler.py
+++ app/main/handler.py
@@ -1,7 +1,38 @@
 from app.main import main_blueprint
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+from app.api.util import get_result_url_by_name
 
 
-@main_blueprint.route('/')
+@main_blueprint.route('', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    try:
+        if request.method == 'POST':
+            name = request.form['name']
+            if name is None or name == '':
+                raise ValueError
+            return redirect(url_for('main.shake', name=name))
+        else:
+            return render_template('index.html')
+    except ValueError:
+        return 'ValueError', 400
+
+
+@main_blueprint.route('shake', methods=['GET', 'POST'])
+def shake():
+    try:
+        if request.method == 'GET':
+            name = request.args.get('name')
+            result, bg = get_result_url_by_name(name)
+            return render_template('shake.html', result=result, bg=bg)
+    except Exception as e:
+        return e, 400
+
+
+@main_blueprint.route('result', methods=['GET'])
+def result():
+    try:
+        return render_template('result.html',
+                               result=request.args.get('result'),
+                               bg=request.args.get('bg'))
+    except Exception as e:
+        return e, 400
