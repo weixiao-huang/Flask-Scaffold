5765a4215ec3f604ec31947193206a97755ab3f5
weixiao-huang <hwx.simle@gmail.com>
2017-06-12 17:53:53 +0800

    final version

--- app/api/util.py
+++ app/api/util.py
@@ -50,7 +50,7 @@ def add_name_and_num(fn, name, num, genera, result_fn, flag=0, genera_id=1):
 
     generaBeginPos = (
         imgSize[0] * 0.7,
-        imgSize[1] * 0.015
+        imgSize[1] * 0.025
     )
 
     numBeginPos = (
