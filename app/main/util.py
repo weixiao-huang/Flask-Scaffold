c7e4f62a570128242876edf181f2e1635bc0ce3d
huangwx <hwx.simle@gmail.com>
2017-06-14 14:21:44 +0800

    all is ok

--- app/main/util.py
+++ app/main/util.py
@@ -32,12 +32,12 @@ def add_name(fn, name, result_fn):
 def get_result_url_by_name_and_id(name, id, pId=None):
     SENTENCES_DIR = os.path.join(app.config['IMG_DIR'], 'sentences', str(id))
     result_fn = str(uuid.uuid1()) + '.png'
-    result_fn = '2.png'
+    # result_fn = '2.png'
     if pId is not None:
         selected_file = str(pId) + '.png'
         return add_name(os.path.join(SENTENCES_DIR, selected_file), name, result_fn), selected_file.split('.png')[0]
     files = os.listdir(SENTENCES_DIR)
     selected_file = files[int(random() * len(files))]
     print(time.strftime("%Y-%m-%d %X", time.localtime()), ': ', selected_file)
-    return add_name(os.path.join(SENTENCES_DIR, selected_file), name, '2.png'), selected_file.split('.png')[0]
+    return add_name(os.path.join(SENTENCES_DIR, selected_file), name, result_fn), selected_file.split('.png')[0]
 
